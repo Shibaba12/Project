e97b6b1be2d66ddde5baf49ad7e8f4e0eb5435f1
task 4 <sarahlin20141224@gmail.com>
2017-11-05 13:11:23 +0200

    timetable

--- screens/TimetableScreen.js
+++ screens/TimetableScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   Image,
   ScrollView,
+  View,
 } from 'react-native';
 import Layout from '../constants/Layout';
 
@@ -15,15 +16,16 @@ export default class TimetableScreen extends React.Component {
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <ScrollView>           
-
-      <Image source={require('../assets/images/timetable.png')} />
-             
-      </ScrollView>
+        <View>
+            <ScrollView horizontal>          
+                <Image
+                source={require('../assets/images/time.jpg')}
+                style={{ height: 371, flex: 1, width: 862 }}
+                />             
+            </ScrollView>
+        </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
   
-});
